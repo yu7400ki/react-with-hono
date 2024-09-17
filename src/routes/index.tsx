b89966fc3c229c77481d110d61154c01de2eacf9
yu7400ki <yu7400ki@gmail.com>
2024-09-18 01:39:32 +0900

    loader

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,23 @@
+import { client } from "@/libs/api";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
+	loader: async ({ abortController }) => {
+		const res = await client.index.$get(
+			{},
+			{ init: { signal: abortController.signal } },
+		);
+		return await res.text();
+	},
 	component: Index,
 });
 
 function Index() {
+	const msg = Route.useLoaderData();
+
 	return (
 		<div>
-			<h3>Welcome Home!</h3>
+			<h3>{msg}</h3>
 		</div>
 	);
 }
