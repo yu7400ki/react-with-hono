1669fa3a09440260125cde5faf3575268f30c82d
yu7400ki <yu7400ki@gmail.com>
2024-09-20 02:12:06 +0900

    `defineConfig`を使わない

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,16 @@
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import type { UserConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig(() => {
-	return {
-		plugins: [TanStackRouterVite(), react(), tsconfigPaths()],
-		server: {
-			proxy: {
-				"/api": {
-					target: "http://localhost:5174",
-					changeOrigin: true,
-				},
+export default {
+	plugins: [TanStackRouterVite(), react(), tsconfigPaths()],
+	server: {
+		proxy: {
+			"/api": {
+				target: "http://localhost:5174",
+				changeOrigin: true,
 			},
 		},
-	};
-});
+	},
+} satisfies UserConfig;
--- vite.server.config.ts
+++ vite.server.config.ts
@@ -1,23 +1,21 @@
 import build from "@hono/vite-build/cloudflare-pages";
 import devServer from "@hono/vite-dev-server";
 import adapter from "@hono/vite-dev-server/cloudflare";
-import { defineConfig } from "vite";
+import type { UserConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig(() => {
-	return {
-		plugins: [
-			build({
-				entry: "api/index.ts",
-			}),
-			devServer({
-				adapter,
-				entry: "api/index.ts",
-			}),
-			tsconfigPaths(),
-		],
-		server: {
-			port: 5174,
-		},
-	};
-});
+export default {
+	plugins: [
+		build({
+			entry: "api/index.ts",
+		}),
+		devServer({
+			adapter,
+			entry: "api/index.ts",
+		}),
+		tsconfigPaths(),
+	],
+	server: {
+		port: 5174,
+	},
+} satisfies UserConfig;
