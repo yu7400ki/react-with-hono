872cc99e5b1532f88d3d7f7b72c655e35469a9ed
yu7400ki <yu7400ki@gmail.com>
2024-09-27 22:34:34 +0900

    fallback

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,14 @@
 import route from "@/route";
+import type { Fetcher } from "@cloudflare/workers-types";
 import { Hono } from "hono";
 
+type Bindings = {
+	ASSETS: Fetcher;
+};
+
 export type AppType = typeof route;
-export default new Hono().route("/api", route);
+export default new Hono<{ Bindings: Bindings }>()
+	.route("/api", route)
+	.notFound((c) => {
+		return c.env.ASSETS.fetch(c.req.raw);
+	});
